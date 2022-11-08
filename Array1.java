24ffb3965f7ee049068b989efd9338d51d654df3
Vish83 <107296266+Vish83@users.noreply.github.com>
2022-11-08 15:54:09 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,15 @@
+package com.arrayEx;
+
+//declare and initialization of array
+
+public class Array1 {
+	public static void main(String[] args) {
+		int[] arr;             //declaration of array
+		int []a;
+		int b [];
+		
+		a=new int[5];
+		
+	}
+
+}
